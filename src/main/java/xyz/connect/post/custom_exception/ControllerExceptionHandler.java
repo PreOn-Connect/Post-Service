fc1452a4eeb8dbdebec6819c6e5f60e1600dd88b
YJ_Lee <ambition65@naver.com>
2023-09-20 19:45:09 +0900

    refactor: 파일 사이즈 초과 예외 응답변경
    - 현재 서버에 설정된 최대 업로드 파일 사이즈를 같이 응답하도록 변경

--- src/main/java/xyz/connect/post/custom_exception/ControllerExceptionHandler.java
+++ src/main/java/xyz/connect/post/custom_exception/ControllerExceptionHandler.java
@@ -2,7 +2,9 @@ package xyz.connect.post.custom_exception;
 
 import com.amazonaws.AmazonServiceException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +17,9 @@ import java.util.stream.Collectors;
 @Slf4j
 @RestControllerAdvice
 public class ControllerExceptionHandler {
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private String maxFileSize;
+
 
     // Api 비즈니스 로직 예외
     @ExceptionHandler(PostApiException.class)
@@ -24,8 +29,9 @@ public class ControllerExceptionHandler {
 
     // 게시글 이미지 용량 초과
     @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public ResponseEntity<ErrorResponse> maxUploadSizeExceededException(MaxUploadSizeExceededException e) {
-        return makeErrorResponseEntity(ErrorCode.MAX_UPLOAD_SIZE_EXCEEDED);
+    public ResponseEntity<ErrorResponse> maxUploadSizeExceededException() {
+        ErrorCode errorCode = ErrorCode.MAX_UPLOAD_SIZE_EXCEEDED;
+        return makeErrorResponseEntity(errorCode, errorCode.getMessage() + " 최대 파일 사이즈: " + maxFileSize);
     }
 
     //API 요청 파라미터 값의 유효성 위반
@@ -44,6 +50,12 @@ public class ControllerExceptionHandler {
     public ResponseEntity<ErrorResponse> amazonServiceExceptionHandler(AmazonServiceException e) {
         log.warn("[AmazonS3 API Exception] " + e.getMessage());
         return makeErrorResponseEntity(ErrorCode.THIRD_PARTY_API_EXCEPTION, e.getMessage());
+    }
+
+    //지원하지 않는 Http method
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> notSupportedMethodExceptionHandler() {
+        return makeErrorResponseEntity(ErrorCode.NOT_SUPPORTED_METHOD);
     }
 
     //알 수 없는 예외
