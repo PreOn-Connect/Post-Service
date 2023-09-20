e7bdcc68cefa7ef6ba46954fea7a4408c77b236c
YJ_Lee <ambition65@naver.com>
2023-09-20 19:27:19 +0900

    feat: 이미지 업로드 관련 예외 처리 추가 #6
    - 파일 업로드 용량 초과
    - 허용되지 않는 미디어 타입
    - AmazonS3 에러

--- src/main/java/xyz/connect/post/custom_exception/ControllerExceptionHandler.java
+++ src/main/java/xyz/connect/post/custom_exception/ControllerExceptionHandler.java
@@ -1,22 +1,31 @@
 package xyz.connect.post.custom_exception;
 
-import java.util.stream.Collectors;
+import com.amazonaws.AmazonServiceException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import xyz.connect.post.enumeration.ErrorCode;
 import xyz.connect.post.web.model.response.ErrorResponse;
 
+import java.util.stream.Collectors;
+
 @Slf4j
-@RestControllerAdvice(basePackages = "xyz.connect.post.web.controller")
+@RestControllerAdvice
 public class ControllerExceptionHandler {
 
     // Api 비즈니스 로직 예외
     @ExceptionHandler(PostApiException.class)
     public ResponseEntity<ErrorResponse> unauthorizedExceptionHandler(PostApiException e) {
         return makeErrorResponseEntity(e.getErrorCode());
+    }
+
+    // 게시글 이미지 용량 초과
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ErrorResponse> maxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        return makeErrorResponseEntity(ErrorCode.MAX_UPLOAD_SIZE_EXCEEDED);
     }
 
     //API 요청 파라미터 값의 유효성 위반
@@ -31,10 +40,17 @@ public class ControllerExceptionHandler {
         return makeErrorResponseEntity(ErrorCode.INVALID_API_PARAMETER, msg);
     }
 
+    @ExceptionHandler(AmazonServiceException.class)
+    public ResponseEntity<ErrorResponse> amazonServiceExceptionHandler(AmazonServiceException e) {
+        log.warn("[AmazonS3 API Exception] " + e.getMessage());
+        return makeErrorResponseEntity(ErrorCode.THIRD_PARTY_API_EXCEPTION, e.getMessage());
+    }
+
     //알 수 없는 예외
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> unknownExceptionHandler(Exception e) {
         log.warn("[Unexpected Error] " + e.getMessage());
+        e.printStackTrace();
         return makeErrorResponseEntity(ErrorCode.SERVER_ERROR);
     }
 
